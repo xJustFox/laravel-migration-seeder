4ce5b5cc272537c27c14a489d9f11c57585d9ce7
xJustFox <dodydaversa@gmail.com>
2024-02-15 14:57:46 +0100

    [Upgrade] Created migration and migrate it to db

---
+++ database/migrations/2024_02_15_130135_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->char('agency', 100);
+            $table->char('departure_station', 200);
+            $table->char('arrival_station', 200);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->char('train_code', 12);
+            $table->tinyInteger('number_carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
