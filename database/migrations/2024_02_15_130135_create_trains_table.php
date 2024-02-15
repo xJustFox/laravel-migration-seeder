2e45e7218061c7c2e58b68e9cae6f78e15704365
xJustFox <dodydaversa@gmail.com>
2024-02-15 15:09:56 +0100

    [Fix] Fixed migrations

--- database/migrations/2024_02_15_130135_create_trains_table.php
+++ database/migrations/2024_02_15_130135_create_trains_table.php
@@ -18,9 +18,11 @@ return new class extends Migration
             $table->char('agency', 100);
             $table->char('departure_station', 200);
             $table->char('arrival_station', 200);
-            $table->dateTime('departure_time');
-            $table->dateTime('arrival_time');
-            $table->char('train_code', 12);
+            $table->date('departure_date');
+            $table->date('arrival_date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->char('train_code', 12)->unique();
             $table->tinyInteger('number_carriages');
             $table->boolean('in_time')->default(true);
             $table->boolean('deleted')->default(false);
